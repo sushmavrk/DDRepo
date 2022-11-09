d6285a98243fda07109ffd7a551ab070c6a1e048
Toshiba <Toshiba@DESKTOP-CSSJGEK>
2022-11-09 21:45:07 +0530

    storing in git

---
+++ src/test/java/Git/Sample.java
@@ -0,0 +1,12 @@
+package Git;
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Storing tests in git");
+		System.out.println("Storing codes in github");
+		
+	}
+
+}
---
+++ src/test/java/Git/Sample1.java
@@ -0,0 +1,12 @@
+package Git;
+
+public class Sample1 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hi world");
+		System.out.println("hello world");
+		
+	}
+
+}
